28c2273e96164e897a155c68ef65dbd8abb9b8c3
Roman Martinez <roman.mx3@gmail.com>
2018-05-15 20:19:46 +1000

    JavaScript transpiling with Babel

--- webpack.config.js
+++ webpack.config.js
@@ -178,6 +178,29 @@ webpackConfig.output = {
 	filename: '[name].js',
 	path: __dirname + '/dist',
 }
+/*
+
+
+Transpiling ES6+ JavaScript
+------------------------------
+1. Install Babel:
+$ npm i --save-dev babel-core				Babel's core functionality for Node.js
+$ npm i --save-dev babel-loader				loader to integrate it with webpack
+$ npm i --save-dev babel-preset-env			preset settings defining which new js features to transpile, and which ones to leave out
+
+2. Add a module rule
+*/
+const babelModule = {
+	test: /\.jsx?$/,
+	exclude: /node_modules/,
+	use: {
+		loader: 'babel-loader',
+		options: {
+			presets: ['env'],
+		},
+	},
+};
+webpackConfig.module.rules.push( babelModule );
 
 // Finally, export the configuration object to make it available for other scripts:
 module.exports = webpackConfig;
